955c6748e7ecb3e054783e894ca248028e512bd6
David <lopezcruzdavid3@gmail.com>
2023-05-19 16:23:35 -0600

    Muchos cambios

--- fut_pred.py
+++ fut_pred.py
@@ -1,4 +1,13 @@
-#bibliotecas a utilizar
+
 import numpy as np
 import pandas as pd
 import math
+
+data = pd.read_excel('Info_clean_2.xlsx')
+data.head(5)
+
+#revision de nulos
+data.info()
+
+#borrando columna llena de nulos 
+del data['Unnamed: 10']
